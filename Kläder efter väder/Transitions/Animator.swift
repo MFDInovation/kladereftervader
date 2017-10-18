7f3d6a5eed080f8abf1deb1a471f00773e1361e4
test <test@Magnuss-MacBook-Pro.local>
2017-10-18 08:53:20 +0200

    animator file fix

--- Kläder efter väder/Transitions/Animator.swift
+++ Kläder efter väder/Transitions/Animator.swift
@@ -1,85 +1,85 @@
-/*
- * Copyright (c) 2014-2016 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+//
+//  Animator.swift
+//  Kläder efter väder
+//
+//  Created by Claes Jacobsson on 2017-10-17.
+//  Copyright © 2017 Knowit. All rights reserved.
+//
 
 import Foundation
 import UIKit
 
-class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
-
-  let duration = 0.3
-  var presenting = true
-  var originFrame = CGRect.zero
-
-  var dismissCompletion: (()->Void)?
-
-  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-    return duration
-  }
-
-  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-    let containerView = transitionContext.containerView
-    let toView = transitionContext.view(forKey: .to)!
-    let detailView = presenting ? toView : transitionContext.view(forKey: .from)!
-
-    let initialFrame = presenting ? originFrame : detailView.frame
-    let finalFrame = presenting ? detailView.frame : originFrame
-
-    let xScaleFactor = presenting ?
-        initialFrame.width / finalFrame.width :
-        finalFrame.width / initialFrame.width
-
-    let yScaleFactor = presenting ?
-        initialFrame.height / finalFrame.height :
-        finalFrame.height / initialFrame.height
-
-    let scaleTransform = CGAffineTransform(scaleX: xScaleFactor, y: yScaleFactor)
-
-    detailView.alpha = presenting ? 0.0 : 1.0
-
-    if presenting {
-        detailView.transform = scaleTransform
-        detailView.center = CGPoint(x: initialFrame.midX, y: initialFrame.midY)
-        detailView.clipsToBounds = true
+class Animator: NSObject, UIViewControllerAnimatedTransitioning {
+    let duration = 0.3
+    var presenting = true
+    var photoView: UIImageView? = nil {
+        didSet {
+            imageFrame = (photoView?.convert((photoView?.bounds)!, to: nil))!
+            image = photoView?.image
+        }
     }
 
-    containerView.addSubview(toView)
-    containerView.bringSubview(toFront: detailView)
+    private var imageFrame = CGRect.zero
+    private var image: UIImage? = nil
 
-    UIView.animate(withDuration: duration,
-                   delay:0.0,
-                   usingSpringWithDamping: 0.7,
-                   initialSpringVelocity: 0.4,
-                   animations: {
-                    detailView.alpha = self.presenting ? 1.0 : 0.0
-                    detailView.transform = self.presenting ? CGAffineTransform.identity : scaleTransform
-                    detailView.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
+    var dismissCompletion: (()->Void)?
 
-    }, completion:{_ in
-        if !self.presenting {
-          self.dismissCompletion?()
-        }
-        transitionContext.completeTransition(true)
-      }
-    )
-  }
-  
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+        let containerView = transitionContext.containerView
+        let fromVC = transitionContext.viewController(forKey: .from)
+        let toVC = transitionContext.viewController(forKey: .to)
+        let fromView = fromVC?.view
+        let toView = toVC?.view
+
+        // Initial and final frame of image animation
+        let initialFrame = presenting ? imageFrame : toView?.frame
+        let finalFrame = presenting ? toView?.frame : imageFrame
+
+        // Create an image view to show image during transition
+        let imageView = UIImageView(frame: initialFrame!)
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = image
+
+        // Hide toView initially
+        toView?.alpha = 0.0
+
+        // Hide detail image scroll view during transition
+        let detailVC = presenting ? toVC as! ImageDetailViewController : fromVC as! ImageDetailViewController
+        detailVC.imageScrollView.isHidden = true
+
+        // Prepare containerView
+        containerView.backgroundColor = UIColor.white
+        containerView.addSubview(toView!)
+        containerView.addSubview(imageView)
+
+        // Hide tapped cell image
+        photoView?.isHidden = true
+
+        UIView.animate(withDuration: duration,
+                       delay:0.0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0.4,
+                       animations: {
+                        fromView?.alpha = 0.0
+                        toView?.alpha = 1.0
+                        imageView.frame = finalFrame!
+        }, completion:{_ in
+
+            imageView.removeFromSuperview()
+
+            if self.presenting {
+                detailVC.imageScrollView.isHidden = false
+            } else {
+                self.dismissCompletion?()
+            }
+
+            transitionContext.completeTransition(true)
+        })
+    }
+
 }
